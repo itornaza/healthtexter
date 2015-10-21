db5e46d8d4da6cfb36c9750e2b6c10687ed86713
Ioannis Tornazakis <itornaza@gmail.com>
2015-10-21 15:01:18 +0300

    Initial Commit

---
+++ healthTexter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  healthTexter
+//
+//  Created by Ioannis Tornazakis on 21/10/15.
+//  Copyright © 2015 polarbear.gr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
